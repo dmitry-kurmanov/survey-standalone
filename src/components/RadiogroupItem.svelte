311ebf1d6358a43bff1e8466e3948a3229240d0e
Dmitriy Kurmanov <kurmanov.work@gmail.com>
2020-11-10 14:13:34 +0300

    work for the radiogroup-item

--- src/components/RadiogroupItem.svelte
+++ src/components/RadiogroupItem.svelte
@@ -1,6 +1,49 @@
 <script>
+	import SurveyString from './String.svelte';
+	import OtherChoice from './OtherChoice.svelte';
+
 	export let element = null;
 	export let index = '';
 	export let item = null;
 	export let hideLabel = false;
+
+	$: labelClass = element.getLabelClass(item);
+	$: controlLabelClass = element.getControlLabelClass(item);
 </script>
+
+<div>
+	<label class={labelClass}>
+		<input
+			type="radio"
+			name={element.name + '_' + element.id}
+			value={item.value}
+			checked={element.renderedValue}
+			id={element.inputId + '_' + index}
+			role="radio"
+			disabled={element.isReadOnly || !item.isEnabled}
+			aria-required={element.isRequired}
+			aria-label={item.locText.renderedHtml}
+			aria-invalid={element.errors.length > 0}
+			aria-describedby={element.errors.length > 0 ? element.id + '_errors' : null}
+			class={element.cssClasses.itemControl} />
+		<span class={element.cssClasses.materialDecorator}>
+			<svg
+				class={element.cssClasses.itemDecorator}
+				viewBox="-12 -12 24 24">
+				<circle r="6" cx="0" cy="0" /></svg>
+		</span>
+		<span class="check" />
+		{#if !hideLabel}
+			<span class={controlLabelClass} title={item.locText.koRenderedHtml}>
+				<SurveyString locString={item.locText} />
+			</span>
+		{/if}
+	</label>
+	<OtherChoice {element} />
+	{#if item.value == element.otherItem.value}
+		<div
+			class:sjs-hide={!element.hasOther || !element.renderedValue || !element.isOtherSelected}>
+			<OtherChoice {element} />
+		</div>
+	{/if}
+</div>
